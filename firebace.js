409d95ecdb40770df58f1d7c30bc472c9803d865
skrsameer <skrsameer0786@gmail.com>
2025-05-03 14:22:16 +0530

    Update firebace.js

--- firebace.js
+++ firebace.js
@@ -1,1 +1,11 @@
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCZ8xdSzT1kNBFn1OKzjmHFE1Y_HRONJ4Q",
+  authDomain: "earn-with-skr-b3eb0.firebaseapp.com",
+  databaseURL: "https://earn-with-skr-b3eb0-default-rtdb.firebaseio.com",
+  projectId: "earn-with-skr-b3eb0",
+  storageBucket: "earn-with-skr-b3eb0.firebasestorage.app",
+  messagingSenderId: "632843327266",
+  appId: "1:632843327266:web:57c5ad6d78fae0ad0b377b",
+  measurementId: "G-M44HFP9M3Y"
+};
